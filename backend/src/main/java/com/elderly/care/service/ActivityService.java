157f72aa1eec2cd5618b40b4d7ebb9fc6f09be3c
lacsar712 <liug07115@gmail.com>
2026-06-19 15:05:06 +0800

    saturn模型提交

--- backend/src/main/java/com/elderly/care/service/ActivityService.java
+++ backend/src/main/java/com/elderly/care/service/ActivityService.java
@@ -1,4 +1,7 @@
 package com.elderly.care.service;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.elderly.care.common.Result;
 import com.elderly.care.entity.Activity;
-public interface ActivityService extends IService<Activity> {}
+public interface ActivityService extends IService<Activity> {
+    Result<?> attend(Long activityId, Long seniorId);
+}
